3571613b2491c9c3f03c25c669378a367ff1de38
EdisonManrique <edison.manrique.chocce@gmail.com>
2023-09-15 11:50:42 -0500

    Singleton JS ES6 creado.

--- 01. Creacionales/01. Singleton/01. ejemplo.js
+++ 01. Creacionales/01. Singleton/01. ejemplo.js
@@ -1,16 +1,21 @@
+// definimos la clase
 class Singleton {
 
+    // creamos la variable para guardar la instancia
     static instance
 
     constructor(){
-        if(this.constructor.instance) return this.constructor.instance
+        // verificamos si hay una instancia ya creada
+        if(this.constructor.instance) return this.constructor.instance // retornamos la instancia previamente creada
+        // si no hay una instancia previa guardamos la instancia
         this.constructor.instance = this
     }
 
 }
 
-
+// creamos dos instancias 
 const instanciaPrincipal = new Singleton()
 const instanciaSecundaria = new Singleton()
 
+// comprobamos si es un singleton
 console.log(`Es un singleton? ${ instanciaPrincipal === instanciaSecundaria }`) // Es un singleton? true
